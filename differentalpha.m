1d897e91e57e3d8ec482f1d5b63b6982ec9fe52c
SSSummery <53185378+SSSummery@users.noreply.github.com>
2020-03-22 01:15:28 +0800

    Create differentalpha.m

---
+++ differentalpha.m
@@ -0,0 +1,8 @@
+%name:differentalpha
+%this code was built in order to show the bound of alpha (Figure 7)
+%pay attention that you should cancle the shield of plot code in myFitCal.m
+
+for i= 0.2:0.01:0.28
+myFitCal(i);
+hold on;
+end
